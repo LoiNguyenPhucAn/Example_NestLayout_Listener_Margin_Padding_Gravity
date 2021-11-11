b1492176794485dc296aef946185cfe9b91a020f
LoiNguyenPhucAn <lnphucan@gmail.com>
2021-11-11 12:37:59 +0700

    v1.1_Lab3.5_11112021_1237
    additional part 3: use dimens.xml buil layout

---
+++ app/src/main/java/com/example/lab35/LayoutPart3Activity.java
@@ -0,0 +1,14 @@
+package com.example.lab35;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LayoutPart3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_part_3);
+    }
+}
